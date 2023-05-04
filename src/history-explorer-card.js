3d2739e02089df546d2207e6b64d18e0d3ab2681
ildar170975 <71872483+ildar170975@users.noreply.github.com>
2023-05-04 04:13:25 +0300

    Update history-explorer-card.js

--- src/history-explorer-card.js
+++ src/history-explorer-card.js
@@ -275,7 +275,7 @@ class HistoryCardState {
 
         let v = this.stateTexts.get(s);
         if( !v ) {
-            v = ( device_class && this._hass.localize(`component.${domain}.state.${device_class}.${state}`) ) || 
+            v = ( device_class && this._hass.localize(`component.${domain}.entity_component.${device_class}.state.${state}`) ) || 
                   this._hass.localize(`component.${domain}.entity_component._.state.${state}`) || state;
             this.stateTexts.set(s, v);
         }
